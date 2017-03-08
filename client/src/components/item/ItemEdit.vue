08a1b094f0c418434e2e190ea23645c56822d34a
Diky Arga <dikyarga.id@gmail.com>
2017-03-08 21:38:00 +0700

    add CRUD Item view

---
+++ client/src/components/item/ItemEdit.vue
@@ -0,0 +1,8 @@
+<template>
+<div class="">
+    <el-button type="primary" @click.native="">back to home</el-button>
+    <br/>
+    <br/>
+    <h2>Edit Item</h2>
+  </div>
+</template>
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,9 +1,18 @@
 import Home from './components/Home.vue'
 import User from './components/user/User.vue'
 import Item from './components/item/Item.vue'
+import ItemDetail from './components/item/ItemDetail.vue'
+import ItemNew from './components/item/ItemNew.vue'
+import ItemAll from './components/item/ItemAll.vue'
+import ItemEdit from './components/item/ItemEdit.vue'
 
 export const routes = [
   { path : '', component: Home},
   { path : '/users', component: User},
-  { path : '/items', component:Item}
+  { path : '/items', component:Item, children: [
+    {path: '', component: ItemAll},
+    {path: 'new', component: ItemNew},
+    {path: ':id', component: ItemDetail},
+    {path: ':id/edit', component: ItemEdit}
+  ]}
 ]
